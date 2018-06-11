277d1c641fbb4707d1f09db0a2066737e8e09cbb
“ljt13609206482 <825067852@qq.com>
2018-06-11 14:28:13 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('style-loader!css-loader!./style/style.css')
+require('./script/load.js')
+require('style-loader!css-loader!./style/font.css')
+
+
+
+function hello(str){
+    alert(str)
+}
+hello('你好123')
---
+++ src/script/load.js
@@ -0,0 +1,5 @@
+function world (){
+    return{
+        
+    }
+}
